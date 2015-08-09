a11f402d8de1d73d5ce4be96a33f883ea9ce59e3
Jason S. McMullan <jason.mcmullan@gmail.com>
2015-08-09 17:36:07 -0400

    simavr: Add map() function
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

--- simavr/Arduino.cpp
+++ simavr/Arduino.cpp
@@ -22,6 +22,11 @@
 static int digitalPin[54];
 static int analogPin[6];
 
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 void pinMode(uint8_t pin, uint8_t mode)
 {
     if (pin >= 54)
