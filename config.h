d14181a911f50d20f93a582d91e274c8105ea975
Jason S. McMullan <jason.mcmullan@gmail.com>
2015-08-09 17:37:55 -0400

    config.h: Reduce the E and Z work volumes (new endstop locations)
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

--- config.h
+++ config.h
@@ -16,12 +16,11 @@
 #define ENABLE_TOOL_FUSER       1
 
 #define SERIAL_SPEED            115200
-//#define SERIAL_SPEED            1000000 /* 1Mbit */
 
 #define X_MM_MAX                650.0
 #define Y_MM_MAX                229.0
-#define Z_MM_MAX                200.0
-#define E_MM_MAX                225.0
+#define Z_MM_MAX                140.0
+#define E_MM_MAX                140.0
 
 #define X_FEED_MAX              2000    /* mm/minute */
 #define Y_FEED_MAX              20000   /* mm/minute */
